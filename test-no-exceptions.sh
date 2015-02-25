db24c4f790dcb656cd20082803f92c4f6ce1cd0a
Roman Ivanov <ivanov-jr@mail.ru>
2015-02-25 15:18:14 -0800

    make script to be independent, from pre-build

--- test-no-exceptions.sh
+++ test-no-exceptions.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-VERSION=6.3-SNAPSHOT
+VERSION=6.4-SNAPSHOT
 
-CHECKSTYLE_REPO=/home/rivanov/checkstyle-test/checkstyle
+#CHECKSTYLE_REPO=/home/rivanov/checkstyle-test/checkstyle
+CHECKSTYLE_REPO=checkstyle
 JAR_ALL=$CHECKSTYLE_REPO/target/checkstyle-$VERSION-all.jar
-
-if [ ! -f $JAR_ALL ]; then
-    echo "File $JAR_ALL not found!"
-    exit 1;
-fi
 
 cd /var/tmp/
 rm -rf checkstyle
@@ -16,9 +12,23 @@ rm -rf guava-libraries
 rm -rf jdk
 
 git clone https://github.com/checkstyle/checkstyle.git
+
+#compile
+cd checkstyle
+mvn clean install -Passembly
+cd ../
+
+if [ ! -f $JAR_ALL ]; then
+    echo "File $JAR_ALL not found!"
+    exit 1;
+fi
+
+# clone testing repos
 git clone https://code.google.com/p/guava-libraries/
 hg clone http://hg.openjdk.java.net/jdk7/jdk7/jdk/
 
+
+#run validation
 java -jar $JAR_ALL \
      -c /google_checks.xml  \
      -o checkstyle-report-checkstyle.txt checkstyle
@@ -33,6 +43,7 @@ java -jar $JAR_ALL \
 
 echo "Grep for Exception:"
 
+# "Got an exception" is from checkstyle/src/main/resources/com/puppycrawl/tools/checkstyle/messages.properties
 grep "Got an exception" checkstyle-report-guava.txt
 grep "Got an exception" checkstyle-report-checkstyle.txt
 grep "Got an exception" checkstyle-report-jdk.txt
